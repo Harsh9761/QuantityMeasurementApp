3054a6c240cc19c717dedef436502c2951b9cb6b
Harsh9761 <harshmathura8@gmail.com>
2026-02-18 16:37:35 +0530

    [Harshdeep Singh]Add. UC2 feature added

---
+++ FeetAndInchMeasurementEquality/InchMeasurement/src/main/java/com/InchMearsurement/InchMeasurement/App.java
@@ -0,0 +1,10 @@
+package com.InchMearsurement.InchMeasurement;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
