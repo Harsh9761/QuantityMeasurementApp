41816909628a1d189efac3fb0fac471f9921b8b7
Harsh9761 <harshmathura8@gmail.com>
2026-03-13 12:26:18 +0530

    [Harshdeep Singh]Add. UC15 N Tier Architecture added

--- src/main/java/com/apps/quantitymeasurement/units/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/units/WeightUnit.java
@@ -1,14 +1,14 @@
-package com.apps.quantitymeasurement;
+package com.apps.quantitymeasurement.units;
 
-public enum VolumeUnit implements IMeasurable {
+public enum WeightUnit implements IMeasurable {
 
-    LITRE(1.0),
-    MILLILITRE(0.001),
-    GALLON(3.78541);
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
 
     private final double conversionFactor;
 
-    VolumeUnit(double conversionFactor) {
+    WeightUnit(double conversionFactor) {
         this.conversionFactor = conversionFactor;
     }
 
